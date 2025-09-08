e4b2645e1226101a648315bec70bdfea22c3c6d7
Raghav Goel <raghavgoel454@gmail.com>
2025-09-08 16:02:47 +0530

    write utils functions

---
+++ backend/src/utils/asynchandler.js
@@ -0,0 +1,15 @@
+// const asyncHandler1 = (requestHandler) => async (req, res, next) => {
+//   try {
+//     await requestHandler(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
+
+const asyncHandler = (requestHandler) => (req, res, next) => {
+  Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+};
+export { asyncHandler };
